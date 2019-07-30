2631b276699815c88b3b5a75db4be73d274cf70f
Tomaszdud <tomasz.dudek1997@gmail.com>
2019-07-30 02:59:08 +0200

    database and app settings

--- Arctowski_project/Arctowski_project/settings.py
+++ Arctowski_project/Arctowski_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'Arctowski_app'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'Arctowski_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Arctowski_db',
+        'USER': 'root',
+        'PASSWORD': 'coderslab',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
