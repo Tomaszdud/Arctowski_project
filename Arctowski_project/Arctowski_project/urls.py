fbc11dc9ff4a4e3637adad88c7d8fd7bee3431f2
Tomaszdud <tomasz.dudek1997@gmail.com>
2019-07-31 00:52:03 +0200

    fix

--- Arctowski_project/Arctowski_project/urls.py
+++ Arctowski_project/Arctowski_project/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('registration/', RegistrationView.as_view()),
     path('login/', LoginView.as_view()),
-    path('logout/' LogoutView.as_view()),
+    path('logout/', LogoutView.as_view()),
 ]
