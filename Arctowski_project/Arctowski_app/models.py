35eac858b7cdb564890ad8775e0f11869720a86d
Tomaszdud <tomasz.dudek1997@gmail.com>
2019-07-31 01:30:11 +0200

    Case and InSace model 1.0

--- Arctowski_project/Arctowski_app/models.py
+++ Arctowski_project/Arctowski_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+STORAGE = (())
+
+
+class Case(models.Model):
+    case_id = models.CharField(max_length=20)
+    owner = models.CharField(max_length=60)
+    type = models.CharField(max_length=50)
+    length = models.IntegerField()
+    width = models.IntegerField()
+    hight = models.IntegerField()
+    weight = models.DecimalField(max_digits=2, decimal_places=1)
+    storage_conditions = models.CharField(choices=STORAGE)
+
+
+class InCase(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=12, decimal_places=1)
+    unit_of_measurement = models.CharField(max_length=50)
+    value = models.DecimalField(max_digits=12, decimal_places=2)
