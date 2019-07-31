7bcacd8255604dd6e62cfd92e178117a1fd99a93
Krzysztof <krzysztofkaczor95@gmail.com>
2019-07-31 22:20:54 +0200

    Add registration and login bootstraped view

--- Arctowski_project/Arctowski_project/settings.py
+++ Arctowski_project/Arctowski_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'Arctowski_app'
+	'Arctowski_app',
+	'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
