42fb2d7a08fa1303b0d1fd052882f61cd8c3455c
Tomaszdud <tomasz.dudek1997@gmail.com>
2019-08-12 22:06:51 +0200

    bug fix

--- Arctowski_project/Arctowski_app/models.py
+++ Arctowski_project/Arctowski_app/models.py
@@ -29,7 +29,7 @@ class Case(models.Model):
     storage_conditions = models.CharField(max_length = 20, choices=STORAGE)
     capacity = models.DecimalField(max_digits=12, decimal_places=2, null=True)
     sum_of_value = models.DecimalField(max_digits=13, decimal_places=2, default=0.00)
-    description = models.CharField(max_length=60)
+    description = models.CharField(max_length=60, null=True, blank=True)
 
     def __str__(self):
         return self.case_id
